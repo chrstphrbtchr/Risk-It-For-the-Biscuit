8c043ab8ca39bdccc28ecd8082d45759990d3c60
chrstphrbtchr <54556324+chrstphrbtchr@users.noreply.github.com>
2025-08-26 18:44:28 -0400

    Throw mechanic.

--- Assets/Scripts/Collideable.cs
+++ Assets/Scripts/Collideable.cs
@@ -2,9 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Collideable : MonoBehaviour
 {
-    bool IsPickedUp = false;
+    public Rigidbody rb;
+    public SpringJoint joint;
+    public ParticleSystem particles;
+    public bool IsPickedUp = false;
+    public float velocityBeforeDestruction;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         if (IsPickedUp)
@@ -17,12 +28,26 @@ public class Collideable : MonoBehaviour
             MouseLaunch.haul = this;
             IsPickedUp=false;
 
-            SpringJoint joint = this.gameObject.AddComponent<SpringJoint>();
+            joint = this.gameObject.AddComponent<SpringJoint>();
 
             joint.connectedBody = collision.collider.GetComponentInParent<Rigidbody>();
             joint.spring = 500;
             joint.minDistance = 0.25f;
             joint.maxDistance = 3;
+        }
+        else
+        {
+            BreakCollideable();
+        }
+    }
+
+    void BreakCollideable()
+    {
+        Debug.Log(rb.velocity.magnitude);
+        if (rb.velocity.magnitude > velocityBeforeDestruction)
+        {
+            Instantiate(particles, this.transform.position, Quaternion.identity);
+            Destroy(this.gameObject);
         }
     }
 }
--- Assets/Scripts/MouseLaunch.cs
+++ Assets/Scripts/MouseLaunch.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 
 public class MouseLaunch : MonoBehaviour
@@ -10,12 +11,16 @@ public class MouseLaunch : MonoBehaviour
     public Transform cam;
     public float strength;
     public static bool IsLauching;
+    public static bool HasRock = true;
+    public static bool IsThrowing;
     float maxTime = 4, timer = 0;
     public float minStrenght, maxStrength;
     public float possibleMaxSpring;
     public static float massMuliplier = 1;
     public static Collideable haul;
     public Transform anchorPoint;
+    public float throwForce = 0, throwMult = 5000;
+    public float maxThrowForce = 1000;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,8 +31,17 @@ public class MouseLaunch : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Launch();
+        Throw();
+        Reel();
+    }
+
+    void Launch()
+    {
+        // LAUNCH
         if (Input.GetMouseButtonDown(0))
         {
+            //mousebody.constraints = RigidbodyConstraints.FreezeAll;
             strength = 10;
         }
         if (Input.GetMouseButton(0))
@@ -37,17 +51,59 @@ public class MouseLaunch : MonoBehaviour
         if (Input.GetMouseButtonUp(0))
         {
             IsLauching = true;
-            //mousebody.constraints = RigidbodyConstraints.FreezeRotation;
+            
             strength = Mathf.Clamp(strength, minStrenght, maxStrength);
             timer = maxTime;
+            //mousebody.constraints = RigidbodyConstraints.FreezeRotation;
             mousebody.AddForce(cam.forward * strength, ForceMode.Impulse);
             //Debug.DrawRay(cam.transform.position, cam.forward, Color.yellow, 5);
         }
+    }
 
+    void Throw()
+    {
+        // THROW BAKED GOODS
+        if (haul != null)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                IsThrowing = true;
+            }
+            if (Input.GetKey(KeyCode.Space))
+            {
+                if (IsThrowing)
+                {
+                    throwForce += Time.deltaTime * throwMult;
+                    throwForce = Mathf.Clamp(throwForce, 1, maxThrowForce);
+                }
+            }
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                if (IsThrowing)
+                {
+                    Destroy(haul.joint);
+                    haul.joint = null;
+                    haul.rb.velocity = Vector3.zero;
+                    haul.rb.AddForce(cam.forward * throwForce, ForceMode.Impulse);
+                    haul.IsPickedUp = false;
+                    haul = null;
+                    IsThrowing = false;
+                    Debug.Log("<color=cyan>So long-a, Bowser!</color>");
+                }
+            }
+        }
+        else
+        {
+            // THROW ROCK
+        }
+    }
+
+    void Reel()
+    {
         if (timer > 0)
         {
             massMuliplier = Mathf.Clamp(massMuliplier, 1, 25);
-            timer -= (Time.deltaTime / massMuliplier );
+            timer -= (Time.deltaTime / massMuliplier);
             timer = Mathf.Clamp(timer, 0, maxTime);
             float nowish = timer / maxTime;
             spring.maxDistance = Mathf.Lerp(0, 55, nowish);

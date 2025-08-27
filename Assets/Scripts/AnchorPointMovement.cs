03b7526d4d95e415632181e31ad48f4d323c0d09
Christopher Butcher <chrstphrbtchr@gmail.com>
2025-08-27 00:47:02 -0400

    Fixed Anchor Point Movement.

--- Assets/Scripts/AnchorPointMovement.cs
+++ Assets/Scripts/AnchorPointMovement.cs
@@ -15,9 +15,15 @@ public class AnchorPointMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.rotation = viewport.rotation;
+        //transform.rotation = viewport.rotation;
         float apx = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
-        float apy = Input.GetAxis("Vertical") * Time.deltaTime * speed;
-        transform.localPosition += new Vector3(apx, 0, apy);
+        float apz = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+
+        Vector3 fwrdRelative = apz * viewport.up;
+        Vector3 rightRelative = apx * viewport.right;
+        Vector3 camRelative = (fwrdRelative + rightRelative);
+        Vector3 finalNewPosition = new Vector3(camRelative.x, 0, camRelative.z);
+        transform.position += finalNewPosition;
+        //transform.localPosition += new Vector3(apx, 0, apz);
     }
 }

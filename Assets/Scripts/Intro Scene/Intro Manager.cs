01fe15aef1e63d94c155317820ecb9b58ff1cdce
Boogdish <MICHAEL@BARTONMUSICALCIRCUITS.COM>
2025-08-30 19:45:58 -0400

    Credits button on opening screen

--- Assets/Scripts/Intro Scene/Intro Manager.cs
+++ Assets/Scripts/Intro Scene/Intro Manager.cs
@@ -9,7 +9,8 @@ public class IntroManager : MonoBehaviour
     public float cubeMoveTime;
     public float delayBetweenCubes;
     public float cubeDistance;
-    public GameObject startButton;
+    public GameObject startButton, creditsScreen;
+    public bool creditsOn;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,11 +26,17 @@ public class IntroManager : MonoBehaviour
             cubes[i].turnOn(cubeMoveTime);
             yield return new WaitForSeconds(delayBetweenCubes);
         }
-        startButton.SetActive(true);
+      //  startButton.SetActive(true);
     }
 
     public void LoadKitchen()
     {
         SceneManager.LoadScene("Kitchen");
+    }
+
+    public void CreditsButton()
+    {
+        if (creditsScreen.activeSelf == false) creditsScreen.SetActive(true);
+        else creditsScreen.SetActive(false);
     }
 }

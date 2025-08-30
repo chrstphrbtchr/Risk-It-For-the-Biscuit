caec20c204830d0320b93b95d0a8e843b15f3639
Boogdish <MICHAEL@BARTONMUSICALCIRCUITS.COM>
2025-08-29 22:51:43 -0400

    Fan Switch / Limiting Anchor Movement

--- Assets/Scripts/AnchorPointMovement.cs
+++ Assets/Scripts/AnchorPointMovement.cs
@@ -6,10 +6,12 @@ public class AnchorPointMovement : MonoBehaviour
 {
     public Transform viewport;
     public float speed;
+    private Vector3 startingPoint;
+    public float maxDistance;
     // Start is called before the first frame update
     void Start()
     {
-        
+        startingPoint = transform.position;
     }
 
     // Update is called once per frame
@@ -33,7 +35,10 @@ public class AnchorPointMovement : MonoBehaviour
         Vector3 rightRelative = apx * viewport.right;
         Vector3 camRelative = (fwrdRelative + rightRelative);
         Vector3 finalNewPosition = new Vector3(camRelative.x, 0, camRelative.z);
-        transform.position += finalNewPosition;
+        Vector3 wantToGo = transform.position + finalNewPosition;
+        Vector3 offset = wantToGo - startingPoint;
+        transform.position = startingPoint + Vector3.ClampMagnitude(offset, maxDistance);
+        //transform.position += finalNewPosition;
         //transform.localPosition += new Vector3(apx, 0, apz);
     }
 }
--- Assets/Scripts/Fan.cs
+++ Assets/Scripts/Fan.cs
@@ -8,17 +8,23 @@ public class Fan : MonoBehaviour
     public bool fanOn, testing;
     public float rotateSpeed, maxSpeed;
     float currentSpeed;
- 
+    public GameObject fanLight;
+
 
     // Update is called once per frame
     void Update()
     {
-        if (fanOn) currentSpeed = Mathf.Clamp(currentSpeed+rotateSpeed, 0, maxSpeed); 
-        else  currentSpeed = Mathf.Clamp(currentSpeed - .01f, 0, 1.5f); 
+
+        if (fanOn) currentSpeed = Mathf.Clamp(currentSpeed + rotateSpeed, 0, maxSpeed);
+        else currentSpeed = Mathf.Clamp(currentSpeed - .01f, 0, 1.5f);
         this.transform.Rotate(0, 0, currentSpeed);
     }
 
+   
+
     public void turnFanOn() => fanOn = true;
-    public void turnFanOff() => fanOn &= false;
+    public void turnFanOff()  
+            {fanOn = false; 
+            fanLight.SetActive(false);}
 
 }
